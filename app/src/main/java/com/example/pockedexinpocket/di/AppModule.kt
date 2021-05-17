de2ccdf29c71da47d1e05fe9528f09dfaeb8b78a
baha <tytenov99@gmail.com>
2021-05-17 18:32:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/pockedexinpocket/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.pockedexinpocket.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object AppModule {
+}
